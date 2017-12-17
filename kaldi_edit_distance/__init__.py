6cf8fcb1caca7b10ec32f83081b4654465f08e82
kamo <naoyuki.kamo829@gmail.com>
2017-12-18 02:41:50 +0900

    Add docstring

--- kaldi_edit_distance/__init__.py
+++ kaldi_edit_distance/__init__.py
@@ -1,3 +1,21 @@
+"""
+Copyright 2009-2011  Microsoft Corporation;  Haihua Xu;  Yanmin Qian
+
+See ../../COPYING for clarification regarding multiple authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+See the Apache 2 License for the specific language governing permissions and
+limitations under the License.
+"""
 from . import edit_distance as ed
 
 
@@ -14,18 +32,51 @@ def _get_ele(seq1, seq2):
     return first_ele
 
 
-def levenshtein_edit_distance(seq1, seq2, details=False):
-    if not isinstance(details, bool):
+def levenshtein_edit_distance(seq1, seq2, detail=False):
+    """
+    (This description comes from kaldi/src/util/edit-distance-inl.h)
+
+    Algorithm:
+        write A and B for the sequences, with elements a_0 ..
+        let |A| = M and |B| = N be the lengths, and have
+        elements a_0 ... a_{M-1} and b_0 ... b_{N-1}.
+        We are computing the recursion
+           E(m, n) = min(  E(m-1, n-1) + (1-delta(a_{m-1}, b_{n-1})),
+                          E(m-1, n),
+                          E(m, n-1) ).
+        where E(m, n) is defined for m = 0..M and n = 0..N and out-of-
+        bounds quantities are considered to be infinity (i.e. the
+        recursion does not visit them).
+
+    We do this computation using a vector e of size N+1.
+    The outer iterations range over m = 0..M.
+
+    Args:
+        seq1 (List[Union[int, float, str]]):
+        seq1 (List[Union[int, float, str]]):
+        detail (bool):
+    Returns:
+        if detail:
+            total (int)
+            insertion (int)
+            deletion (int),
+            substitution (int)
+        else:
+            total (int)
+    """
+    if not isinstance(detail, bool):
         raise TypeError('details arg must have bool type')
+    seq1 = tuple(seq1)
+    seq2 = tuple(seq2)
     ele = _get_ele(seq1, seq2)
 
     if isinstance(ele, int):
-        if details:
+        if detail:
             return ed.levenshtein_edit_distance_detail_int(seq1, seq2)
         else:
             return ed.levenshtein_edit_distance_int(seq1, seq2)
     elif isinstance(ele, float):
-        if details:
+        if detail:
             return ed.levenshtein_edit_distance_detail_float(seq1, seq2)
         else:
             return ed.levenshtein_edit_distance_float(seq1, seq2)
@@ -33,7 +84,7 @@ def levenshtein_edit_distance(seq1, seq2, details=False):
         if isinstance(ele, str):
             seq1 = [i.encode() for i in seq1]
             seq2 = [i.encode() for i in seq2]
-        if details:
+        if detail:
             return ed.levenshtein_edit_distance_detail_bytes(seq1, seq2)
         else:
             return ed.levenshtein_edit_distance_bytes(seq1, seq2)
@@ -43,6 +94,20 @@ def levenshtein_edit_distance(seq1, seq2, details=False):
 
 
 def levenshtein_alignment(seq1, seq2, eps_symbol):
+    """
+
+    Args:
+        seq1 (List[T]):
+        seq1 (List[T]):
+        eps_symbol (T):
+    Returns:
+        total (int):
+        aligned1 (List[T]):
+        aligned2 (List[T]):
+    """
+    seq1 = tuple(seq1)
+    seq2 = tuple(seq2)
+
     ele = _get_ele(seq1, seq2)
     if not isinstance(eps_symbol, type(ele)):
         raise TypeError('eps_symbol must have {} type'.format(type(ele)))
